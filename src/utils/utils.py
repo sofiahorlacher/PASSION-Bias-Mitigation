ac94e2ee32d37d52dcb3339942776609e311fbea
Sofia Horlacher <ohsolomio5@gmail.com>
2026-03-10 07:39:18 +0100

    fix seeding

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -15,6 +15,10 @@ def fix_random_seeds(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
+    torch.use_deterministic_algorithms(True)
 
 def set_requires_grad(model, val):
     for p in model.parameters():
